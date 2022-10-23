7544d6deee09efac9a62b7652c352a549927756d
Diego Camino Reinoso <diego.camino.r@gmail.com>
2022-10-23 03:03:59 -0500

    Add struct project to crowdfunding contract

--- CrowdFundingPlatzi.sol
+++ CrowdFundingPlatzi.sol
@@ -3,21 +3,17 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract CrowdfundingPlatzi {
-    string public id;
-    string public name;
-    string public description;
-    address payable public author;
-    uint public state = 1;
-    uint public funds;
-    uint public fundraisingGoal;
-
-    constructor (string memory _id, string memory _name, string memory _description, uint _fundraisingGoal) {
-        id = _id;
-        name = _name;
-        description = _description;
-        fundraisingGoal = _fundraisingGoal;
-        author = payable(msg.sender);
+    struct Project {
+        string id;
+        string name;
+        string description;
+        address payable author;
+        uint state;
+        uint funds;
+        uint fundraisingGoal;
     }
+
+    Project public project;
 
     event ProjectFunded(
         string projectId,
@@ -29,13 +25,21 @@ contract CrowdfundingPlatzi {
         uint state
     );
 
+
+
+    constructor (string memory _id, string memory _name, string memory _description, uint _fundraisingGoal) {
+        project = Project(_id, _name, _description, payable(msg.sender), 1, 0, _fundraisingGoal);
+    }
+
+
+
     modifier isNotAuthor() {
-        require(msg.sender != author, "As author you can not fund your own project");
+        require(msg.sender != project.author, "As author you can not fund your own project");
         _;
     }
 
     modifier isAuthor() {
-        require(msg.sender == author, "You need to be the project owner");
+        require(msg.sender == project.author, "You need to be the project owner");
         _;
     }
 
@@ -43,15 +47,15 @@ contract CrowdfundingPlatzi {
 
     function foundProject() public payable isNotAuthor {
         require(msg.value > 0, "Fund value must be greater than 0");
-        require(state == 1, "The crowdfunding is closed, you can not contribute anymore to the project");
-        author.transfer(msg.value);
-        funds += msg.value;
-        emit ProjectFunded(id, msg.value);
+        require(project.state == 1, "The crowdfunding is closed, you can not contribute anymore to the project");
+        project.author.transfer(msg.value);
+        project.funds += msg.value;
+        emit ProjectFunded(project.id, msg.value);
     }
 
     function changeProjectState(uint newState) public isAuthor {
-        require(state != newState, "New state must be different");
-        state = newState;
-        emit ProjectStateChanged(id, newState);
+        require(project.state != newState, "New state must be different");
+        project.state = newState;
+        emit ProjectStateChanged(project.id, newState);
     }
 }
